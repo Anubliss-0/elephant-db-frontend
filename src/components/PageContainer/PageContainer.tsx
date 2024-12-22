96778d7ef4d2b00bc8c95d0ca6026c1452dc050c
Thomas Rice <thomas.rotterdam@protonmail.com>
2024-12-22 19:23:33 +0100

    PageContainer accepting fullWdith argument

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -3,13 +3,14 @@ import { useTheme } from '../../contexts/ThemeContext'
 import classNames from 'classnames'
 
 type PageContainerProps = {
-    children: React.ReactNode
+    children: React.ReactNode,
+    fullWidth?: boolean
 }
 
-function PageContainer({ children }: PageContainerProps) {
+function PageContainer({ children, fullWidth = false }: PageContainerProps) {
     const { theme } = useTheme()
     return <div className={classNames(styles.pageContainer, styles[theme])}>
-        <div className={classNames(styles.pageContent, styles[theme])}>
+        <div className={classNames(styles.pageContent, styles[theme], { [styles.fullWidth]: fullWidth })}>
             {children}
         </div>
     </div>
